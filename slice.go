40878a771e40d7de2bb2d6fd73ccf71d4590fd9e
zeindevs <zeindevs@gmail.com>
2024-07-25 14:39:04 +0700

    add slice

---
+++ slice.go
@@ -0,0 +1,32 @@
+package gome
+
+import "strings"
+
+func stringInSlice(pattern string, slice []string, caseSensitive bool) bool {
+	if !caseSensitive {
+		pattern = strings.ToLower(pattern)
+	}
+
+	for _, entry := range slice {
+		if !caseSensitive {
+			entry = strings.ToLower(entry)
+		}
+		if entry == pattern {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Contains returns true if the specified string slice contains the
+// specified string (case-sensitive).
+func SliceContains(slice []string, pattern string) bool {
+	return stringInSlice(pattern, slice, true)
+}
+
+// ContainsNoCase returns true if the specified string slice contains the
+// specified string (case-insensitive).
+func SliceContainsNoCase(slice []string, pattern string) bool {
+	return stringInSlice(pattern, slice, false)
+}
